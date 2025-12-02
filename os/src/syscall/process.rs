a5841264eb26650317958e5051513084c444237f
Just1nWu <1295360720@qq.com>
2025-12-02 20:03:52 +0800

    feat: 实现了sys_trace id为0与1的情况。

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -41,5 +41,13 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => unsafe { (_id as *const u8).read_volatile() as isize },
+        1 => {
+            unsafe { (_id as *mut u8).write_volatile(_data as u8) };
+            0
+        }
+        2 => -1,
+        _ => -1,
+    }
 }
